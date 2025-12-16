497183470123d989f7d3f738bcdf4e38f26af6b9
Intars Voitkevičs <intars.voitkevics@sk.lvt.lv>
2025-12-16 09:42:52 +0200

    Ieviesta izlozes loģika

--- loterija/src/loterija/Loterija.java
+++ loterija/src/loterija/Loterija.java
@@ -1,10 +1,48 @@
 package loterija;
 
+import java.util.Collections;
+import java.util.Random;
+import java.util.Stack;
+
+import javax.swing.JOptionPane;
+
 public class Loterija {
-
+	static Random rand = new Random();
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String izvele;
+		Stack<Integer> winbumb = new Stack<>();
+		Stack<Integer> trauks = new Stack<>();
+		//String darbibas, lai nākotnē varētu ieviest 60s waittime
+		String[] darbibas = {"Sākt izlozi", "Apturēt"};
+		
+		
+		do {
+				izvele = (String) JOptionPane.showInputDialog(null, "Izvēlies darbību", "Darbību saraksts", 
+						JOptionPane.QUESTION_MESSAGE, null, darbibas, darbibas[0]);
+				
+		if(izvele == null)
+			izvele = "Apturēt";
+		
+		switch (izvele) {
+		case "Sākt izlozi":
+			for(int i = 0; i<3; i++) {
+				for(int j= 0; j<10; j++) {
+				trauks.push(rand.nextInt(10)+0);
+					Collections.shuffle(trauks);
+							winbumb.push(trauks.peek());
+						}
+			}
+		
+			JOptionPane.showMessageDialog(null, "Tavi laimīgais skaitlis:  "+winbumb.pop(), "Paziņojums", JOptionPane.INFORMATION_MESSAGE);
+			
+			break;
+			
+		case  "Apturēt":
+			JOptionPane.showMessageDialog(null, "Programma apturēta!");
+			break;
+		}
+		}while(!izvele.equals("Apturēt"));
+	
 	}
 
 }
