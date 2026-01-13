0f6ced91999d1da8ff5f282c2c0db81b8c9e1798
Intars Voitkevičs <intars.voitkevics@sk.lvt.lv>
2026-01-13 10:07:50 +0200

    Programmas pirmsākumi.

---
+++ pica/src/pica/PicasEka.java
@@ -0,0 +1,17 @@
+package pica;
+
+public class PicasEka {
+
+	public static void main(String[] args) {
+		int picLiel;
+		String piedevas, merces;
+		boolean VaiSavaktUzVietas;
+		
+		double cena;
+		
+		// picas ID būs random.
+		
+		// Ja skolotājs šo skatīsies, pašlaik pierakstu idejas telefonā. Nopietni.
+	}
+
+}
